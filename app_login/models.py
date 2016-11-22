da67aca792efc8b9629e1d0ef10e97c8702e9471
PythonCakeMing <1851464029@qq.com>
2016-11-22 14:51:33 +0800

    login views

--- app_login/models.py
+++ app_login/models.py
@@ -1,3 +1,11 @@
+import datetime
 from django.db import models
 
+
 # Create your models here.
+class Manager(models.Model):
+    name = models.CharField(max_length=11)
+    phone = models.CharField(max_length=11)
+    code = models.CharField(max_length=6)
+    update_time = models.DateTimeField(default=datetime.datetime.now())
+
--- app_login/views.py
+++ app_login/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from utils_common.message_sender import send_message
+from utils_common.message_sender import check_user
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'login.html')
+
+
+def get_code(request, phone):
+    ret = send_message(phone)
+    if not ret:
+        return JsonResponse({'error': 1})
+    return JsonResponse({'error': 0})
+
+
+def check_code(request, phone, code):
+    ret = check_user(phone, code)
+    if not ret:
+        return JsonResponse({'error': 1})
+    return JsonResponse({'error': 0})
