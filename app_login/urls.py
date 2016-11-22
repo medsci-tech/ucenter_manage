218e7a4cba85f4449b0dab4cad9d3dbb1680b571
PythonCakeMing <1851464029@qq.com>
2016-11-22 15:52:04 +0800

    auth logic

--- app_login/urls.py
+++ app_login/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^get_code/(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/$', views.get_code),
-    url(r'^check_code/(?P<phone>[0-9]+)/(?P<code>[0-9]+)$', views.check_code)
 ]
