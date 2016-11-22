7f6351681f68fc1642197adf1ad84260ab11f72c
PythonCakeMing <1851464029@qq.com>
2016-11-22 11:15:54 +0800

    refactor project

--- ucenter_manage/wsgi.py
+++ ucenter_manage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ucenter_statistic project.
+WSGI config for ucenter_manage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ucenter_statistic.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ucenter_manage.settings")
 
 application = get_wsgi_application()
