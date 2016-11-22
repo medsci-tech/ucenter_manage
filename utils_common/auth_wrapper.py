4b4fa9663029184a169ee917bfc8a3eb9324763a
PythonCakeMing <1851464029@qq.com>
2016-11-22 15:30:13 +0800

    auth wrapper

---
+++ utils_common/auth_wrapper.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseRedirect
+
+
+def is_login(request):
+    user_phone = request.session.get('user_phone', False)
+    if user_phone:
+        return True
+    return False
+
+
+def auth_wrapper(function):
+    def inner(request, *arg, **kwargs):
+        if is_login(request):
+            temp_ret = function(request, *arg, **kwargs)
+            return temp_ret
+        else:
+            return HttpResponseRedirect('/login/')
+
+    return inner
