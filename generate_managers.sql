f03d81a294fd14a8fd7a73bec4f7c8fde5819683
PythonCakeMing <1851464029@qq.com>
2016-11-22 16:26:08 +0800

    auth sqlite

---
+++ generate_managers.sql
@@ -0,0 +1,30 @@
+insert into app_login_manager
+(id, name, phone, code, update_time)
+values
+(3, '柳俊', '15827388360', '0', datetime('now', 'localtime'));
+
+insert into app_login_manager
+(id, name, phone, code, update_time)
+values
+(4, '蔡乾', '18671616161', '0', datetime('now', 'localtime'));
+
+insert into app_login_manager
+(id, name, phone, code, update_time)
+values
+(5, '陈凯', '13801273648', '0', datetime('now', 'localtime'));
+
+insert into app_login_manager
+(id, name, phone, code, update_time)
+values
+(6, '吴欣江', '18627187569', '0', datetime('now', 'localtime'));
+
+
+insert into app_login_manager
+(id, name, phone, code, update_time)
+values
+(7, '邓迎', '13810732647', '0', datetime('now', 'localtime'));
+
+insert into app_login_manager
+(id, name, phone, code, update_time)
+values
+(8, '丁工物', '18611610269', '0', datetime('now', 'localtime'));
