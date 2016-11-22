71900c8ec90fd872b5ef43582db15464a4186d89
PythonCakeMing <1851464029@qq.com>
2016-11-22 15:48:53 +0800

    auth logic

--- app_login/views.py
+++ app_login/views.py
@@ -7,23 +7,24 @@ from utils_common.message_sender import check_user
 
 # Create your views here.
 def index(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    phone = request.POST.get('phone')
+    code = request.POST.get('code')
+    ret = check_user(phone, code)
+    if not ret:
+        return JsonResponse({'error': 1, 'msg': 'check user code error'})
+    request.session['user_phone'] = phone
+    return JsonResponse({'error': 0})
 
 
 def get_code(request, phone):
     key = send_message(phone)
     if not key:
-        return JsonResponse({'error': 1})
+        return JsonResponse({'error': 1, 'msg': 'send message error'})
 
     ret = update_user(phone, key)
     if not ret:
-        return JsonResponse({'error': 1})
+        return JsonResponse({'error': 1, 'msg': 'get user error'})
 
-    return JsonResponse({'error': 0})
-
-
-def check_code(request, phone, code):
-    ret = check_user(phone, code)
-    if not ret:
-        return JsonResponse({'error': 1})
     return JsonResponse({'error': 0})
