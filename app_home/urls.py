91ce54d119f5479526edfb7959c3348a70b8dbd7
PythonCakeMing <1851464029@qq.com>
2016-11-22 10:30:09 +0800

    home views & urls

---
+++ app_home/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^year_user/(?P<year>\d{4})/$', views.home_year_user),
+    url(r'^month_user/(?P<year>\d{4})/$', views.home_month_user),
+    url(r'^day_user/(?P<year>\d{4})/(?P<month>[1-9]|1[0-2])/$', views.home_day_user),
+    url(r'^all_bean/$', views.home_all_bean),
+    url(r'^year_bean/(?P<year>\d{4})/$', views.home_year_bean),
+    url(r'^month_bean/(?P<year>\d{4})/$', views.home_month_bean),
+    url(r'^day_bean/(?P<year>\d{4})/(?P<month>[1-9]|1[0-2])/$', views.home_day_bean)
+]
+
+
+
+
---
+++ app_home/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from utils_analyzer.user_analyzer import all_user
+from utils_analyzer.user_analyzer import year_user
+from utils_analyzer.user_analyzer import month_user
+from utils_analyzer.user_analyzer import day_user
+from utils_analyzer.bean_analyzer import all_bean
+from utils_analyzer.bean_analyzer import year_bean
+from utils_analyzer.bean_analyzer import month_bean
+from utils_analyzer.bean_analyzer import day_bean
+
+
+# Create your views here.
+def index(request):
+    ret = all_user()
+    print('*' * 10)
+    print(ret)
+
+    return HttpResponse('index')
+
+
+def home_year_user(request, year):
+    ret = year_user(int(year))
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_year_user')
+
+
+def home_month_user(request, year):
+    ret = month_user(int(year))
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_month_user')
+
+
+def home_day_user(request, year, month):
+    ret = day_user(int(year), int(month))
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_day_user')
+
+
+def home_all_bean(request):
+    ret = all_bean()
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_all_bean')
+
+
+def home_year_bean(request, year):
+    ret = year_bean(int(year))
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_year_bean')
+
+
+def home_month_bean(request, year):
+    ret = month_bean(int(year))
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_month_bean')
+
+
+def home_day_bean(request, year, month):
+    ret = day_bean(int(year), int(month))
+    print('*' * 10)
+    print(ret)
+    return HttpResponse('home_day_bean')
+
+
+
+
+
