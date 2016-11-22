916b7725b4a0835c6ca85687acfbe24b3d60f9eb
PythonCakeMing <1851464029@qq.com>
2016-11-22 16:34:09 +0800

    auth views

--- app_login/views.py
+++ app_login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.http import HttpResponseRedirect
 from utils_common.message_sender import send_message
 from utils_common.message_sender import update_user
 from utils_common.message_sender import check_user
@@ -7,15 +8,7 @@ from utils_common.message_sender import check_user
 
 # Create your views here.
 def index(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    phone = request.POST.get('phone')
-    code = request.POST.get('code')
-    ret = check_user(phone, code)
-    if not ret:
-        return JsonResponse({'error': 1, 'msg': 'check user code error'})
-    request.session['user_phone'] = phone
-    return JsonResponse({'error': 0})
+    return render(request, 'login.html')
 
 
 def get_code(request, phone):
@@ -28,3 +21,11 @@ def get_code(request, phone):
         return JsonResponse({'error': 1, 'msg': 'get user error'})
 
     return JsonResponse({'error': 0})
+
+
+def check_code(request, phone, code):
+    ret = check_user(phone, code)
+    if not ret:
+        return JsonResponse({'error': 1, 'msg': 'check error'})
+    request.session['user_phone'] = phone
+    return HttpResponseRedirect('/home/')
