a42d6e7741ac8b3b33728f9c49e77dbc4b37b662
PythonCakeMing <1851464029@qq.com>
2016-11-22 14:37:41 +0800

    json response

--- app_users/urls.py
+++ app_users/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<year>\d{4})/$', views.index),
+    url(r'^$', views.index),
+    url(r'^(?P<year>\d{4})/$', views.users_year_user),
     url(r'^month_user/(?P<year>\d{4})/$', views.users_month_user),
     url(r'^day_user/(?P<year>\d{4})/(?P<month>[1-9]|1[0-2])/$', views.users_day_user),
     url(r'^year_bean/(?P<year>\d{4})/$', views.users_year_bean),
--- app_users/views.py
+++ app_users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 from utils_analyzer.user_analyzer import year_user
 from utils_analyzer.user_analyzer import month_user
 from utils_analyzer.user_analyzer import day_user
@@ -10,43 +10,41 @@ from utils_analyzer.bean_analyzer import day_bean
 
 # Create your views here.
 def index(request, year):
-    ret = year_user(int(year), user_role='user')
-    print('*' * 10)
-    print(ret)
     return render(request, 'users.html')
+
+
+def users_year_user(request, year):
+    ret = year_user(int(year), user_role='user')
+    response = JsonResponse(ret, safe=False)
+    return response
 
 
 def users_month_user(request, year):
     ret = month_user(int(year), user_role='user')
-    print('*' * 10)
-    print(ret)
-    return HttpResponse('users_month_user')
+    response = JsonResponse(ret, safe=False)
+    return response
 
 
 def users_day_user(request, year, month):
     ret = day_user(int(year), int(month), user_role='user')
-    print('*' * 10)
-    print(ret)
-    return HttpResponse('users_day_user')
+    response = JsonResponse(ret, safe=False)
+    return response
 
 
 def users_year_bean(request, year):
     ret = year_bean(int(year), user_role='user')
-    print('*' * 10)
-    print(ret)
-    return HttpResponse('users_year_bean')
+    response = JsonResponse(ret, safe=False)
+    return response
 
 
 def users_month_bean(request, year):
     ret = month_bean(int(year), user_role='user')
-    print('*' * 10)
-    print(ret)
-    return HttpResponse('users_month_bean')
+    response = JsonResponse(ret, safe=False)
+    return response
 
 
 def users_day_bean(request, year, month):
     ret = day_bean(int(year), int(month), user_role='user')
-    print('*' * 10)
-    print(ret)
-    return HttpResponse('users_day_bean')
+    response = JsonResponse(ret, safe=False)
+    return response
 
