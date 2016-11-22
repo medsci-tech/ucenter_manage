6cf08914918eb296374bde29cab79d198729c873
PythonCakeMing <1851464029@qq.com>
2016-11-22 10:35:53 +0800

    add check route

--- app_manage/views.py
+++ app_manage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Manager
+from utils_common.message_sender import send_message
+from utils_common.message_sender import check_user
 
 
 # Create your views here.
@@ -12,4 +14,18 @@ def index(request):
     ret = Manager.objects.all()
     for user in ret:
         print(user.name)
-    return HttpResponse('home_day_bean')
+    return HttpResponse('index')
+
+
+def get_code(phone):
+    ret = send_message(phone)
+    if not ret:
+        pass
+    return HttpResponse('get_code')
+
+
+def check_code(phone, code):
+    ret = check_user()
+    if not ret:
+        pass
+    return HttpResponse('check_code')
