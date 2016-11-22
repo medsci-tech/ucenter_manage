428674bc02f15f607bf2db4a51c59820925c6039
PythonCakeMing <1851464029@qq.com>
2016-11-22 14:53:41 +0800

    refactor

--- app_doctors/urls.py
+++ app_doctors/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^(?P<year>\d{4})/$', views.doctors_year_user),
+    url(r'^year_user/(?P<year>\d{4})/$', views.doctors_year_user),
     url(r'^month_user/(?P<year>\d{4})/$', views.doctors_month_user),
     url(r'^day_user/(?P<year>\d{4})/(?P<month>[1-9]|1[0-2])/$', views.doctors_day_user),
     url(r'^year_bean/(?P<year>\d{4})/$', views.doctors_year_bean),
--- app_users/urls.py
+++ app_users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^(?P<year>\d{4})/$', views.users_year_user),
+    url(r'^year_user/(?P<year>\d{4})/$', views.users_year_user),
     url(r'^month_user/(?P<year>\d{4})/$', views.users_month_user),
     url(r'^day_user/(?P<year>\d{4})/(?P<month>[1-9]|1[0-2])/$', views.users_day_user),
     url(r'^year_bean/(?P<year>\d{4})/$', views.users_year_bean),
