70d5711c720010fe522f068a5e4bed007cd70409
余鹏飞 <501254061@qq.com>
2016-11-28 15:37:45 +0800

    test

--- static/js/charts/character_info.js
+++ static/js/charts/character_info.js
@@ -7,19 +7,24 @@ var vm_character_info = new Vue({
             width: 'auto',
             height: 250
         },
-        get_url: '/',
+        get_url: 'info/',
     },
     methods: {
         is_show: function(e) {
             if (typeof(e) == 'object') {
                 return false;
-            } 
+            }
             return true;
         }
     },
     compiled: function() {
 
-        this.data = data;
+        var vm = this;
+
+        $.get(this.get_url, {}, function(data) {
+            vm.get_data = data;
+        });
+
     }
 
 });
