742be9723c1bbbb818a8abb5ca04858c2e7754be
zhaiyu <zhaiyu_hb@hotmail.com>
2016-11-28 17:51:10 +0800

    add excel export

--- utils_common/django_excel.py
+++ utils_common/django_excel.py
@@ -1,11 +1,43 @@
 # pyexcel_xlsx
 
-from pyexcel_xlsx import save_data
+import xlwt
+
+from django.http import HttpResponse
+
+def import_response(filename=None):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="%s.xls"' % filename
+    return response
 
 
-def excel_export(filename, data):
-    # data = [[1, 2, 'f'], [4, 5, 6], [7, 8, 9]]
-    sheetx = {
-        "huge": data
-    }
-    return save_data('%s.xlsx' % filename, sheetx)
+def excel_export(columns, rows):
+    # response = HttpResponse(content_type='application/ms-excel')
+    # response['Content-Disposition'] = 'attachment; filename="users.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Users')
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    # columns = ['Username', 'First name', ]
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    # rows = User.objects.filter(role='user').values_list('phone', 'role')[:10]
+
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    return wb
+    # wb.save(response)
+    # return response
