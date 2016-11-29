074b4bb08f0673a0e904bd4c8ee31fe9c57e7b4b
zhaiyu <zhaiyu_hb@hotmail.com>
2016-11-29 15:44:56 +0800

    修改app_characters url 正则

--- app_characters/urls.py
+++ app_characters/urls.py
@@ -2,12 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/$', views.index),
-    url(r'^(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/info/$', views.info),
-    url(r'^(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/characters/$', views.characters),
-    url(r'^(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/projects/$', views.projects),
-    url(r'^(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/year_character/(?P<year>\d{4})/$',
-        views.user_year_character),
-    url(r'^(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/month_character/(?P<year>\d{4})/$',
-        views.user_month_character)
+    url(r'^(?P<phone>(1[3578]\d{9}))/$', views.index),
+    url(r'^(?P<phone>(1[3578]\d{9}))/info/$', views.info),
+    url(r'^(?P<phone>(1[3578]\d{9}))/characters/$', views.characters),
+    url(r'^(?P<phone>(1[3578]\d{9}))/projects/$', views.projects),
+    url(r'^(?P<phone>(1[3578]\d{9}))/year_character/(?P<year>\d{4})/$', views.user_year_character),
+    url(r'^(?P<phone>(1[3578]\d{9}))/month_character/(?P<year>\d{4})/$', views.user_month_character)
 ]
