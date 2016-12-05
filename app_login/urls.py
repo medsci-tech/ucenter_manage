ff6c30f887f71bbf57482e1fd03b42f9de4a1716
zhaiyu <zhaiyu_hb@hotmail.com>
2016-12-05 11:34:34 +0800

    login

--- app_login/urls.py
+++ app_login/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^login/$', views.login, name='login'),
     url(r'^get_code/(?P<phone>(1[3578]\d{9}))/$', views.get_code),
-    url(r'^logout/(?P<phone>(1[3578]\d{9}))/$', views.logout),
+    url(r'^logout/(?P<phone>(1[3578]\d{9}))/$', views.logout, name='logout'),
     url(r'^check_code/(?P<phone>[0-9]+)/(?P<code>[A-Za-z0-9]{6})$', views.check_code)
 ]
