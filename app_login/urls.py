400321936bee88578968f82ad078003e172d6a4f
Ifeinstein <501254061@qq.com>
2016-12-05 10:42:01 +0800

    restart authentication

--- app_login/urls.py
+++ app_login/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^get_code/(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/$', views.get_code),
-    url(r'^logout/(?P<phone>((13[0-9]{1})|(15[0-9]{1})|(17[0-9]{1})|(18[0-9]{1}))+\d{8})/$', views.logout),
+    url(r'^get_code/(?P<phone>(1[3578]\d{9}))/$', views.get_code),
+    url(r'^logout/(?P<phone>(1[3578]\d{9}))/$', views.logout),
     url(r'^check_code/(?P<phone>[0-9]+)/(?P<code>[A-Za-z0-9]{6})$', views.check_code)
 ]
