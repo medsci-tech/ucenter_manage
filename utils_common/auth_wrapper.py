9995d9d10eeb0ec8f37e1f28c32fabb6e5d1724d
fishyu <501254061@qq.com>
2017-03-29 11:32:03 +0800

    comment out auth_wrapper

--- utils_common/auth_wrapper.py
+++ utils_common/auth_wrapper.py
@@ -2,18 +2,19 @@ from django.http import HttpResponseRedirect
 
 
 def is_login(request):
-    user_phone = request.session.get('user_phone', False)
-    if user_phone:
-        return True
-    return False
-    # return True
+    # user_phone = request.session.get('user_phone', False)
+    # if user_phone:
+    #     return True
+    # return False
+    return True
     
 
 def auth_wrapper(function):
     def inner(request, *arg, **kwargs):
-        if is_login(request):
-            temp_ret = function(request, *arg, **kwargs)
-            return temp_ret
-        else:
-            return HttpResponseRedirect('/')
+        pass
+        # if is_login(request):
+        #     temp_ret = function(request, *arg, **kwargs)
+        #     return temp_ret
+        # else:
+        #     return HttpResponseRedirect('/')
     return inner
